e174ae07379f02e8d28658746071a5a9aebc546d
Real-Septicake <septicake056@gmail.com>
2025-03-16 00:35:22 -0400

    first attempted implementation of malloc and free, not tested but looks pretty

--- src/lib/stdlib/malloc.c
+++ src/lib/stdlib/malloc.c
@@ -12,6 +12,17 @@ int compar(const void *ina, const void *inb);
 
 // The chunk in which allocations will live
 Chunk biggest;
+uint32_t base_addr;
+uint32_t top;
+
+typedef struct _Block {
+    size_t len;
+    char used;
+    struct _Block *last,
+        *next; // doubly linked list to allow for defragging in both directions
+} Block;
+
+Block *head = NULL;
 
 void init_malloc() {
     // last entry is repeat of first
@@ -30,10 +41,100 @@ void init_malloc() {
         if (chunks[i].len_lower > biggest.len_lower)
             biggest = chunks[i];
     }
+    base_addr = biggest.base_lower;
+    top = base_addr;
 }
 
 Chunk *getBiggest() {
     return &biggest;
+}
+
+void *sbrk(intptr_t inc) {
+    // no modifications to top until check passes
+    if (top + inc - base_addr > biggest.len_lower)
+        return (void *)-1;
+    uint32_t ret = top;
+    top += inc;
+    return (void *)ret;
+}
+
+void *malloc(size_t size) {
+    size_t full_size = size + sizeof(Block);
+    if (head) {
+        Block *last = NULL;
+        Block *curr = head;
+        // the loop breaks if it hits the end of the list, the current node can
+        // hold a new node on top of the requested memory, or the node fits the
+        // requested memory exactly. Any other break conditions run the risk of
+        // slowly losing memory
+        while (curr && (curr->len < full_size || curr->len != size)) {
+            last = curr;
+            curr = curr->next;
+        }
+
+        if (curr) {
+            if (curr->len == size) {
+                curr->used = 1;
+                return curr + 1; // return the address right after the block
+            } else {
+                void *ptr = curr + 1;
+                curr->len -= full_size;
+                ptr += curr->len;
+                Block *header = (Block *)ptr;
+                header->len = size;
+                header->used = 1;
+
+                header->next = curr->next;
+                if (curr->next)
+                    curr->next->last = header;
+                header->last = curr;
+                curr->next = header;
+            }
+        } else {
+            void *ptr = sbrk(full_size);
+            if (ptr == (void*)-1) {
+                return NULL;
+            }
+            Block *header = (Block *)ptr;
+            header->last = last;
+            last->next = header;
+            header->next = NULL;
+            header->used = 1;
+            header->len = size;
+            head = header;
+            return ptr + sizeof(Block);
+        }
+    } else {
+        void *ptr = sbrk(full_size);
+        if (ptr == (void*)-1) {
+            return NULL;
+        }
+        Block *header = (Block *)ptr;
+        header->last = NULL;
+        header->next = NULL;
+        header->used = 1;
+        header->len = size;
+        head = header;
+        return ptr + sizeof(Block);
+    }
+    return NULL;
+}
+
+void free(void *ptr) {
+    Block *header = ptr - sizeof(Block);
+    // probably needs a better check, but it's nearly midnight
+    if (header->used != 1)
+        return;
+
+    header->used = 0;
+    if (header->next && !header->next->used) {
+        header->len += header->next->len + sizeof(Block);
+        header->next = header->next->next;
+    }
+    if (header->last && !header->last->used) {
+        header->last->len += header->len + sizeof(Block);
+        header->last->next = header->next;
+    }
 }
 
 // sorts first by type, then by base address
