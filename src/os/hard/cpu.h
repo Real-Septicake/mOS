9f1238c9473f77a9db1d729476189a475055ef57
Septicake <septicake056@gmail.com>
2025-10-03 18:42:39 -0400

    Registers being dumped

--- src/os/hard/cpu.h
+++ src/os/hard/cpu.h
@@ -1,16 +1,21 @@
 #ifndef CPU_H
 #define CPU_H
 
-    //long rax = 0, rbx = 0, rcx = 0, rdx = 0, rsi = 0, rdi = 0, rsp = 0, rbp = 0;
-
+    //
 #define DUMP() do{ \
-    short rip = 0;\
-    __asm__ volatile ("mov %0, %%ax" : "=r"(rip)); \
+    long eax = 0, ebx = 0, ecx = 0, edx = 0, esi = 0, edi = 0, esp = 0, ebp = 0;\
+    long eip = 0;\
+    (void)(eax+ebx+ecx+edx+esi+edi+esp+ebp+eip);\
+    __asm__ volatile ("mov %%eax, %0\nmov %%ebx, %1\n" \
+            "mov %%ecx, %2\nmov %%edx, %3\nmov %%esi, %4" : \
+            "=r"(eax), "=r"(ebx), "=r"(ecx), "=r"(edx), "=r"(esi)); \
     char buff[256];\
-    int len = snprintf(buff, sizeof(buff), "rip=%i", rip);\
-    while (!serialWriteReady(COM1))\
-        ;\
-    serialWrite(COM1, (uint8_t *)buff, len);\
+    (void)snprintf(buff, sizeof(buff), "eax=%i ebx=%i ecx=%i edx=%i esi=%i", eax, ebx, ecx, edx, esi);\
+    writeText(buff, 0, 23, white);\
+    __asm__ volatile ("mov %%edi, %0\nmov %%esp, %1\nmov %%ebp, %2" \
+            : "=r"(edi), "=r"(esp), "=r"(ebp)); \
+    (void)snprintf(buff, sizeof(buff), "edi=%i esp=%i epb=%i", edi, esp, ebp);\
+    writeText(buff, 0, 24, white);\
 }while(0)
 
 #endif
